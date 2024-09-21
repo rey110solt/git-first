19cc44e68afab084be5ce92342dfce4f788b2214
rey110solt <75928843+rey110solt@users.noreply.github.com>
2024-09-21 22:55:47 +0330

    a java code is added

---
+++ java/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args){
+      String name = "Reyhaneh";
+      String family ="Soltanshah";
+      int age = 25;
+      float grade = 17.2f;
+      char first_char = 'r';
+      double mark = 14.2568957;
+      Long number= 25000000000L;
+      System.out.println(name);
+      System.out.println(family);
+      System.out.println(age);
+      System.out.println(grade);
+      System.out.println(first_char);
+      System.out.println(mark);
+      System.out.println(number);
+    }
+}
